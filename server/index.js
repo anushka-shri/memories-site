95185506d1f4ec012ff5a7192178105088a37014
Anushka Shrivastava <anushka.shrivastavaa@gmail.com>
2021-04-13 19:47:37 +0530

    routes added

--- server/index.js
+++ server/index.js
@@ -3,22 +3,27 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-// import postRoutes from './routes/posts.js';
+import postRoutes from './routes/posts.js'
+
+
 
 const app = express();
+
+
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-// app.use('/posts', postRoutes);
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL =
-	'mongodb+srv://user:userpassword@cluster0.gcksh.mongodb.net/Cluster0?retryWrites=true&w=majority';
+	'mongodb+srv://user:userpassword@cluster0.gcksh.mongodb.net/memoriessite?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose
-	.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+	.connect(CONNECTION_URL,
+		{ useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() =>
 		app.listen(PORT, () =>
 			console.log(`Server Running on Port:${PORT}`),
