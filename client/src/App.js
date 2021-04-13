ef558126e8f6123dde8b6a05119ec49e1265c034
Anushka Shrivastava <anushka.shrivastavaa@gmail.com>
2021-04-13 21:09:18 +0530

    client folder setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from 'react';
+import {
+    Container,
+    AppBar,
+    Typography,
+    Grow,
+    Grid
+} from '@material-ui/core';
+import memories from './images/memories.jpg'
 
 function App() {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
-    )
+			<Container maxWidth='lg'>
+				<AppBar position='static' color='inherit'>
+					<Typography variant='h2' align='center'>
+						Memories
+					</Typography>
+					<img src={memories} alt='memories' height='60' />
+				</AppBar>
+				<Grow in>
+					<Container container justify='space-between' alignItems='stretch' spacing={3}>
+						<Grid>
+							<Grid xs={12} sm={7}>
+								<Posts />
+							</Grid>
+							<Grid xs={12} sm={4}>
+								<Form />
+							</Grid>
+						</Grid>
+					</Container>
+				</Grow>
+			</Container>
+		);
 }
 
 export default App
