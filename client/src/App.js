c3bf6502be6e46fc609ed93f77056b0800bb51c2
Anushka Shrivastava <anushka.shrivastavaa@gmail.com>
2021-04-14 08:17:51 +0530

    reducers setup

--- client/src/App.js
+++ client/src/App.js
@@ -7,15 +7,20 @@ import {
     Grid
 } from '@material-ui/core';
 import memories from './images/memories.jpg'
+import Posts from './components/posts/Posts';
+import Form from './components/Form/Form';
+import useStyles from './styles';
+
 
 function App() {
+	const classes = useStyles();
     return (
 			<Container maxWidth='lg'>
-				<AppBar position='static' color='inherit'>
-					<Typography variant='h2' align='center'>
+				<AppBar className={classes.appBar} position='static' color='inherit'>
+					<Typography className={classes.heading} variant='h2' align='center'>
 						Memories
 					</Typography>
-					<img src={memories} alt='memories' height='60' />
+				<img className={classes.image} src={memories} alt='memories' height='60' />
 				</AppBar>
 				<Grow in>
 					<Container container justify='space-between' alignItems='stretch' spacing={3}>
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore, compose } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers'
+
 
 import App from './App';
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
 	<React.StrictMode>
