a52e137e3f11573c4d0355e8f576adc63b36adb7
Anushka Shrivastava <anushka.shrivastavaa@gmail.com>
2021-05-02 01:27:07 +0530

    Form updated

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,13 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useStyles from './styles';
+import FileBase from './react-file-base64';
+import { TextField, Button, Typography, Paper } from '@material-ui/core';
 
 function Form() {
-    const classes = useStyles();
-    return (
-        <div>
-            <h1>Form</h1>
-        </div>
-    )
+	const [postData, setPostData] = useState({
+		creator: '',
+		title: '',
+		message: '',
+		tags: '',
+		selectedFile: '',
+	});
+
+	const classes = useStyles();
+
+	const handleSubmit = () => {};
+	return (
+		<Paper className={classes.paper}>
+			<form
+				autoComplete='off'
+				noValidate
+				className={classes.form}
+				onSubmit={handleSubmit}>
+				<Typography variant='h6'>Add Memory Here</Typography>
+				<TextField
+					name='creator'
+					label='Creator'
+					variant='outlined'
+					fullWidth
+					onChange={(e) =>
+						setPostData({ ...postData, creator: e.target.value })
+					}
+					value={postData.creator}
+				/>
+				<TextField
+					name='titler'
+					label='Title'
+					variant='outlined'
+					fullWidth
+					onChange={(e) => setPostData({ ...postData, title: e.target.value })}
+					value={postData.creator}
+				/>
+				<TextField
+					name='message'
+					label='Message'
+					variant='outlined'
+					fullWidth
+					onChange={(e) =>
+						setPostData({ ...postData, message: e.target.value })
+					}
+					value={postData.creator}
+				/>
+				<TextField
+					name='tags'
+					label='Tags'
+					variant='outlined'
+					fullWidth
+					onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
+					value={postData.creator}
+				/>
+				<div className={classes.fileInput}>
+					<FileBase
+						type='file'
+						multiple={false}
+						onDone={({ base64 }) =>
+							setPostData({ ...postData, selectedFile: base64 })
+						}
+					/>
+				</div>
+				<Button
+					className={classes.buttonSubmit}
+					variant='contained'
+					color='primary'
+					size='large'
+					type='submit'
+					fullWidth>
+					Submit
+				</Button>
+				<Button
+					variant='contained'
+					color='secondary'
+					size='small'
+					onClick={clear}
+					fullWidth>
+					Clear
+				</Button>
+			</form>
+		</Paper>
+	);
 }
 
-export default Form
+export default Form;
--- server/index.js
+++ server/index.js
@@ -3,13 +3,9 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-import postRoutes from './routes/posts.js'
-
-
+import postRoutes from './routes/posts.js';
 
 const app = express();
-
-
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
@@ -22,13 +18,9 @@ const CONNECTION_URL =
 const PORT = process.env.PORT || 5000;
 
 mongoose
-	.connect(CONNECTION_URL,
-		{ useNewUrlParser: true, useUnifiedTopology: true })
+	.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() =>
-		app.listen(PORT, () =>
-			console.log(`Server Running on Port:${PORT}`),
-			
-		),
+		app.listen(PORT, () => console.log(`Server Running on Port:${PORT}`)),
 	)
 	.catch((error) => console.log(`${error} did not connect`));
 
