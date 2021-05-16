587f9919888ad9fcbffd02c076723bd3e6cdbd03
Anushka Shrivastava <anushka.shrivastavaa@gmail.com>
2021-05-17 00:02:23 +0530

    input file

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
 	Paper,
 	Typography,
+	TextField,
 	Toolbar,
 	Avatar,
 	Button,
@@ -13,26 +14,32 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import useStyles from './styles';
 
 function Auth() {
-    const classes = useStyles();
-    const isSignUp = false;
+	const classes = useStyles();
+	const isSignUp = false;
 
-    const handleSubmit = () => {
-        
-    }
+	const handleSubmit = () => {};
+	const handleChange = () => {};
 	return (
 		<Container maxWidth='xs' component='main'>
 			<Paper className={classes.paper} elevation={3}>
 				<Avatar className={classes.avatar}>
 					<LockOutlinedIcon />
 				</Avatar>
-                <Typography variant='h5'>
-                    {isSignUp ? 'Sign Up' : 'Sign In'}
-                </Typography>
-                <form className={classes.form} onSubmit={handleSubmit}>
-                    <Grid>
-                        
-                    </Grid>
-                </form>
+				<Typography variant='h5'>{isSignUp ? 'Sign Up' : 'Sign In'}</Typography>
+				<form className={classes.form} onSubmit={handleSubmit}>
+					<Grid container spacing={2}>
+						{isSignUp && (
+							<>
+								<TextField
+									name='firstName'
+									label='First Name'
+									handleChange={handleChange}
+									autoFocus
+								/>
+							</>
+						)}
+					</Grid>
+				</form>
 			</Paper>
 		</Container>
 	);
