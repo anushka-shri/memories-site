e63283d764515cece3a99b71664399737f5412e2
Anushka Shrivastava <anushka.shrivastavaa@gmail.com>
2021-05-20 00:13:54 +0530

    auth Middleware

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,1 +1,21 @@
 import mongoose from 'mongoose';
+
+const userSchema = mangoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	id: {
+		type: String,
+	},
+});
+
+export default mongoose.model('User', userSchema);
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,5 @@
 import express from 'express';
-import {
-	
-} from '../controllers/postControllers.js';
+import { signin, signup } from '../controllers/userControllers.js';
 
 const router = express.Router();
 
